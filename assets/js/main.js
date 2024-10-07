1e0f5a119c7d9b517e2f0a38cedc2b89fbbb0af6
jahir <supersimple.jahir@gmail.com>
2024-10-07 14:39:18 +0600

    [Done] Hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-
+// scroll to top
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  if (window.scrollY > 50) {
+    // You can adjust the 50px as needed
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
