91dabe6f255845bc332bd764c7dd2998cf2b3535
jahir <supersimple.jahir@gmail.com>
2024-10-08 18:26:14 +0600

    [Done] all section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,30 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // 640: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
